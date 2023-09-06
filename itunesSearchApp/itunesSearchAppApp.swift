64db490e07e2f6c34e48c85e279694d4cb8cc3c8
Alexander Zarutskiy <sashasolo23@gmail.com>
2023-09-06 15:53:50 +0100

    Initial Commit

---
+++ itunesSearchApp/itunesSearchAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  itunesSearchAppApp.swift
+//  itunesSearchApp
+//
+//  Created by Alexander Zarutskiy on 06.09.2023.
+//
+
+import SwiftUI
+
+@main
+struct itunesSearchAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
